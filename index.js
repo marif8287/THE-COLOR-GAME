d208ee145a00020f2f45659fc0368619a69f5a99
Mohd Arif <94686742+mohdarifcpp@users.noreply.github.com>
2022-08-11 12:53:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,35 @@
 let noOfSquares = 9;
-
 let squares = [];
 
-for(let i=0;i<9;i++){
-
+for (let i = 0; i < 9; i++) {
     squares.push(document.querySelectorAll(".square")[i]);
-
 }
 
 // to show random colours on opening tha game
 createColor(squares);
 
 let pickedColor = pickColor();
-
 document.querySelector(".rgb").innerHTML = pickedColor.toUpperCase();
 
 // event listener for new colors button
 
-document.querySelector(".new-color").addEventListener("click", function(){
+document.querySelector(".new-color").addEventListener("click", function () {
 
     createColor(squares);
     pickedColor = pickColor();
     document.querySelector(".rgb").innerHTML = pickedColor.toUpperCase();
-
     document.querySelector(".message").innerHTML = "CHOOSE THE RIGHT COLOR";
-
-    
 })
 
 // eventlistener for easy button
 
-document.querySelector(".easy").addEventListener("click", function(){
+document.querySelector(".easy").addEventListener("click", function () {
 
     noOfSquares = 3;
     pickedColor = pickColor();
     document.querySelector(".rgb").innerHTML = pickedColor.toUpperCase();
 
-    for(let i=noOfSquares;i<9;i++){
+    for (let i = noOfSquares; i < 9; i++) {
 
         document.querySelectorAll(".square")[i].style.visibility = "hidden";
     }
@@ -44,17 +37,17 @@ document.querySelector(".easy").addEventListener("click", function(){
 
 // eventlistener for medium button
 
-document.querySelector(".medium").addEventListener("click", function(){
+document.querySelector(".medium").addEventListener("click", function () {
 
     noOfSquares = 6;
     pickedColor = pickColor();
     document.querySelector(".rgb").innerHTML = pickedColor.toUpperCase();
-    
-    for(let i=0;i<noOfSquares;i++){
+
+    for (let i = 0; i < noOfSquares; i++) {
 
         document.querySelectorAll(".square")[i].style.visibility = "visible";
     }
-    for(let i=noOfSquares;i<9;i++){
+    for (let i = noOfSquares; i < 9; i++) {
 
         document.querySelectorAll(".square")[i].style.visibility = "hidden";
     }
@@ -62,14 +55,13 @@ document.querySelector(".medium").addEventListener("click", function(){
 
 // eventlistener for hard button
 
-document.querySelector(".hard").addEventListener("click", function(){
+document.querySelector(".hard").addEventListener("click", function () {
 
     noOfSquares = 9;
     pickedColor = pickColor();
     document.querySelector(".rgb").innerHTML = pickedColor.toUpperCase();
-    
-    for(let i=0;i<noOfSquares;i++){
 
+    for (let i = 0; i < noOfSquares; i++) {
         document.querySelectorAll(".square")[i].style.visibility = "visible";
     }
 })
@@ -77,65 +69,49 @@ document.querySelector(".hard").addEventListener("click", function(){
 
 // eventListener to perform action while we click on a particular div;
 
-for(let i=0;i<noOfSquares;i++){
+for (let i = 0; i < noOfSquares; i++) {
 
-document.querySelectorAll(".square")[i].addEventListener("click",function(){
+    document.querySelectorAll(".square")[i].addEventListener("click", function () {
 
-    if(this.style.backgroundColor == pickedColor){
-
-        for(let i=0;i<noOfSquares;i++){
-
-            document.querySelectorAll(".square")[i].style.backgroundColor = pickedColor;
+        if (this.style.backgroundColor == pickedColor) {
+            for (let i = 0; i < noOfSquares; i++) {
+                document.querySelectorAll(".square")[i].style.backgroundColor = pickedColor;
+            }
+            document.querySelector(".message").innerHTML = "CONGRAGULATIONS 🤩🥳 , YOU SELECTED THE RIGHT COLOR !!";
         }
 
-        document.querySelector(".message").innerHTML = "CONGRAGULATIONS❤️❤️ , YOU SELECTED THE RIGHT COLOR !!";
-
-    }
-
-    else{
-
-        this.style.backgroundColor = "black";
-    }
-
-})
+        else {
+            this.style.backgroundColor = "black";
+        }
+    })
 
 }
 
 // function to fill colors in the box
 
-function createColor(squares){
+function createColor(squares) {
 
-    
-        for(let i=0;i<noOfSquares;i++){
-
-            squares[i].style.backgroundColor = randomColour();
-        }
-
+    for (let i = 0; i < noOfSquares; i++) {
+        squares[i].style.backgroundColor = randomColour();
+    }
 }
 
 // function to generate a random color
 
-function randomColour(){
+function randomColour() {
 
     let r = Math.round(Math.random() * 256);
     let g = Math.round(Math.random() * 256);
     let b = Math.round(Math.random() * 256);
 
-    let colour = "RGB("+ r + "," + g + "," + b + ")";
-
+    let colour = "RGB(" + r + "," + g + "," + b + ")";
     return colour;
 }
 
 // to get the target box color
 
-function pickColor(){
+function pickColor() {
 
     let divNo = Math.round(Math.random() * noOfSquares);
-
     return (document.querySelectorAll(".square")[divNo].style.backgroundColor);
-
 }
-
-
-
-
